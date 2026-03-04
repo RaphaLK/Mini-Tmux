964591a326b87d9d838cfd5da29bddaff13090a6
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2026-03-04 15:37:24 -0800

    comments, header, window size payload attributes

--- include/protocol.h
+++ include/protocol.h
@@ -8,16 +8,30 @@
 #define MAX_SESSIONS 5
 #define BUFFER_SIZE 4096
 
+// message type defintion
 enum class MsgType : uint8_t {
   CREATE_SESSION = 1,
   ATTACH_SESSION = 2,
-  DETACH = 3,
+  DETACH = 3, // close session, keep pty alive
   LIST_SESSION = 4,
   DATA = 5,
-  WINSIZE_CHANGE = 6,
+  WINSIZE_CHANGE = 6, // TIOCSWINSZ
   SESSION_LIST = 7,
   ERROR_RESP = 8,
   OK_RESP = 9
+};
+
+// message header definition, ensure packing - 9 bytes: 1 byte type, 4 bytes session id, 4 bytes payload_len
+struct __attribute__((packed)) MsgHeader {
+  MsgType type;
+  uint32_t session_id;
+  uint32_t payload_len;
+};
+
+// 
+struct __attribute__((packed)) WinsizePayload {
+  uint16_t rows; // horizontal lines  
+  uint16_t cols; // chars per line
 };
 
 #endif
