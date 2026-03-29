48bc5607d056b3b3f142b51ceb2ee9f6ee38b0d3
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2026-03-29 14:32:33 -0700

    pty_utils.h and gitignore

---
+++ include/pty_utils.h
@@ -0,0 +1,16 @@
+#ifndef PTY_UTILS_H
+#define PTY_UTILS_H
+
+#include <termios.h>
+#include <sys/types.h>
+#include <cstdint>
+
+int create_pty_session(pid_t *child_pid);
+
+void set_winsize(int fd, uint16_t rows, uint16_t cols);
+
+void set_raw_mode(int fd, struct termios *saved);
+
+void restore_term(int fd, struct termios *saved);
+
+#endif
